36a32b2051644940ce42e1bab311a181d874a70e
Rylan Lorance <rylancl@gmail.com>
2021-03-08 11:28:26 -0600

    Created Basic UI components MainView

--- CS_SS_AeroReader_v1.0/View/SupervisedMainView.py
+++ CS_SS_AeroReader_v1.0/View/SupervisedMainView.py
@@ -1,8 +1,8 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
 
 
 class SupervisedMainView(QWidget):
@@ -11,8 +11,28 @@ class SupervisedMainView(QWidget):
         self.mainVBox = QVBoxLayout()
         self.setLayout(self.mainVBox)
 
-        l1 = QLabel()
-        l1.setText("Supervised Search Algorithm")
+        self.formGroupBox = QGroupBox(title="MyGroupBox")
+        self.fbox = QFormLayout()
+        self.fbox.setAlignment(Qt.AlignLeft)
+        self.__createFormBox()
+        self.mainVBox.addWidget(self.formGroupBox)
 
-        self.mainVBox.addWidget(l1)
-        self.mainVBox.addStretch()
+    def __createFormBox(self):
+        self.l1 = QLabel("Search here")
+
+        self.searchBar = QLineEdit()
+        self.l1.setAlignment(Qt.AlignLeft)
+        self.l1.setAlignment(Qt.AlignBottom)
+
+        self.fbox.addRow(self.l1, self.searchBar)
+
+        self.searchButton = QPushButton("Search")
+        self.searchButton.clicked.connect(self.__searchButtonClicked)
+        self.fbox.addRow(self.searchButton)
+
+        self.formGroupBox.setLayout(self.fbox)
+
+    def __searchButtonClicked(self):
+        searchString = self.searchBar.text()
+        print("Working with the following : [{0}]".format(searchString))
+        searchString.lower()

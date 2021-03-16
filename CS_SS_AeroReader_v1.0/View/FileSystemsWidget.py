c406f0540ff0f293457359576560ebbb397baf88
Rylan Lorance <rylancl@gmail.com>
2021-03-16 13:29:13 -0500

    Setting Up FileSystemsWidget.py

--- CS_SS_AeroReader_v1.0/View/FileSystemsWidget.py
+++ CS_SS_AeroReader_v1.0/View/FileSystemsWidget.py
@@ -1,6 +1,7 @@
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QFileDialog, QBoxLayout, \
-    QHBoxLayout
+    QHBoxLayout, QGroupBox, QFrame
+from PyQt5.uic.properties import QtGui
 
 
 class FileSystemsDialogueWidget(QWidget):
@@ -9,16 +10,36 @@ class FileSystemsDialogueWidget(QWidget):
         self.parent = parent
         self.v_layout = QVBoxLayout()
 
-        # click button to open file systems
-        self.btn = QPushButton("Q File Open")
-        self.btn.clicked.connect(self.__getFiles)
-        self.btn.resize(60, 60)
+        self.addDocumentsBox = QGroupBox("Add Documents")
+        self.addDocumentsBox.setFixedSize(400, 500)
+        self.addDocumentsBoxLayout = QVBoxLayout()
 
-        self.v_layout.addWidget(self.btn, alignment=QtCore.Qt.AlignTop)
+        self.addDocumentsBox.setLayout(self.addDocumentsBoxLayout)
+        self.buildDocumentsBoxUI()
 
-        self.__createBtn()
+        self.v_layout.addWidget(self.addDocumentsBox)
+
+        # self.v_layout.addWidget(self.btn, alignment=QtCore.Qt.AlignTop)
 
         self.setLayout(self.v_layout)
+
+    def buildDocumentsBoxUI(self):
+        print("Building Docs Box")
+        docs_box = QVBoxLayout()
+        container = QWidget()
+        container.setLayout(docs_box)
+        container.setFixedSize(200, 200)
+        container.setStyleSheet("background-color: grey")
+
+        l1 = Qt.QLabel("Drag and Drop (Under Construction)")
+
+        btn = Qt.QPushButton("Click to browse files")
+        btn.clicked.connect(self.__getFiles)
+
+        docs_box.addWidget(l1)
+        docs_box.addWidget(btn)
+        self.addDocumentsBoxLayout.addWidget(container, alignment=QtCore.Qt.AlignHCenter)
+
 
     def __getFiles(self):
         print("Getting File systems")
@@ -34,19 +55,6 @@ class FileSystemsDialogueWidget(QWidget):
                 data = f.read()
                 print(data)
 
-    def __createBtn(self):
-        self.h_box = QHBoxLayout()
-        self.nextScreenBtn = QPushButton("Next Screen")
-        self.nextScreenBtn.clicked.connect(self.__fs_goToNextScreen)
-        # self.nextScreenBtn.clicked.connect(super.goToNextScreen())
-        self.previousScreenBtn = QPushButton("Previous Screen")
-
-        self.h_box.addWidget(self.nextScreenBtn, alignment=QtCore.Qt.AlignBottom)
-        self.h_box.addWidget(self.previousScreenBtn)
-        # self.v_layout.addLayout(self.h_box)
-
     def __fs_goToNextScreen(self):
         print("Going to next screen local")
         self.parent.goToNextScreen()
-
-

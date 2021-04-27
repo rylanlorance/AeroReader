b3ef94a7900333263395b1152ca12a702bb23ec7
timbrahm <55643902+timbrahm@users.noreply.github.com>
2021-04-26 20:33:43 -0500

    Update main.py

--- CS_SS_AeroReader_v1.0/bert/main.py
+++ CS_SS_AeroReader_v1.0/bert/main.py
@@ -1,36 +1,37 @@
 import time
 
-from bert.bert_search import BertSearch
+from bert_search import BertSearch
 
 
 def main():
+    clusters = [["angle", "hat", "body"], ["chair", "death", "life"]]
     init_start = time.perf_counter()
-    bert = BertSearch(max_seq=100)
+    bert = BertSearch(max_seq=200)
     init_end = time.perf_counter()
     init_total = init_end - init_start
     print(f"BERT init time: {init_total}\n")
 
     search_start = time.perf_counter()
-    result = bert.search("fish")
+    result = bert.search("fish", clusters)
     search_end = time.perf_counter()
     search_total = search_end - search_start
     print(result)
     print(f"Search time: {search_total}\n")
 
-    search_start = time.perf_counter()
-    result = bert.search("clothes")
-    search_end = time.perf_counter()
-    search_total = search_end - search_start
-    print(result)
-    print(len(result))
-    print(f"Search time: {search_total}\n")
-
-    search_start = time.perf_counter()
-    result = bert.search("fish")
-    search_end = time.perf_counter()
-    search_total = search_end - search_start
-    print(result)
-    print(f"Search time: {search_total}\n")
+    # search_start = time.perf_counter()
+    # result = bert.search("clothes", clusters)
+    # search_end = time.perf_counter()
+    # search_total = search_end - search_start
+    # print(result)
+    # print(len(result))
+    # print(f"Search time: {search_total}\n")
+    #
+    # search_start = time.perf_counter()
+    # result = bert.search("fish", clusters)
+    # search_end = time.perf_counter()
+    # search_total = search_end - search_start
+    # print(result)
+    # print(f"Search time: {search_total}\n")
 
     return 0
 
